fb970aa0c8698c66714fab346e0ed9c1917fbb42
DonManzano91 <alex.batt99@gmail.com>
2021-07-24 15:29:19 -0500

    exp. metodos, bug save, bug categoria null

--- src/main/java/com/manzano/market/dominio/servicio/ProductService.java
+++ src/main/java/com/manzano/market/dominio/servicio/ProductService.java
@@ -11,7 +11,7 @@ import java.util.Optional;
 @Service
 public class ProductService {
 
-    @Autowired
+    @Autowired(required = true)
     private ProductRepository productRepository;
 
     public List<Product> getAll(){
--- src/main/java/com/manzano/market/web/ProductController.java
+++ src/main/java/com/manzano/market/web/ProductController.java
@@ -4,8 +4,7 @@ import com.manzano.market.dominio.Product;
 import com.manzano.market.dominio.repositorio.ProductRepository;
 import com.manzano.market.dominio.servicio.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -14,26 +13,30 @@ import java.util.Optional;
 @RequestMapping("/products")
 public class ProductController {
 
-    @Autowired
+    @Autowired(required = true)
     private ProductService productService;
 
+    @GetMapping("/all")
     public List<Product> getAll(){
         return productService.getAll();
     }
 
-    public Optional<Product> getProduct(int productId){
+    @GetMapping("/{id}")
+    public Optional<Product> getProduct(@PathVariable("id") int productId){
         return productService.getProduct(productId);
     }
-
-    public Optional<List<Product>> getByCategory(int categoryId){
+    @GetMapping("/category/{idCategory}")
+    public Optional<List<Product>> getByCategory(@PathVariable("idCategory") int categoryId){
         return productService.getByCategory(categoryId);
     }
 
-    public Product save(Product product){
+    @PostMapping("/save")
+    public Product save(@RequestBody Product product){
         return productService.save(product);
     }
 
-    public boolean delete(int productId){
+    @DeleteMapping("/delete/{idDelete}")
+    public boolean delete(@PathVariable("idDelete") int productId){
         return productService.delete(productId);
     }
 
